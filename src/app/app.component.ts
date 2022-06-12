f9fa1d95c2674deff7385678b91b1f0bc3b032d8
FernandoAlarcon <erickfernando_20@hotmail.com>
2022-06-12 11:25:44 -0500

    Square App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,83 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'square';
-}
+
+  lorem : string = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Tenetur modi numquam quasi fugit aliquid corrupti quod magnam sint, molestias, dolore mollitia. 
+  Dolorum similique deleniti fugit ab. Commodi veritatis eum adipisci.`;  
+
+  title    : string = 'square';
+  color    : string = '';
+  position : string = "";
+  squares  : any = [
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" }, 
+    { "color"       : "red" } 
+
+  ];
+
+  changeColor(){
+    let number = parseInt(this.position)
+    this.squares[number].color = this.color;
+    this.color    = '';
+    this.position = '';
+
+  } 
+  ChooseSquare(place:any){
+    this.position = place;
+  } 
+  //
+
+  AlertColor(){
+    alert("You Didn't choose the position"); 
+  }
+
+}//AppComponent
